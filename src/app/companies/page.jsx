6daf02ded759ddd16c9cd830af2d206fd2ada791
mostafijur1126 <mostafijurrhaman87@gmail.com>
2026-06-03 21:15:41 +0600

    test commit

--- src/app/companies/page.jsx
+++ src/app/companies/page.jsx
@@ -1,7 +1,12 @@
 import React from "react";
 
 const companiesPage = () => {
-  return <div>companiesPage</div>;
+  return (
+    <div>
+      companiesPage
+      <h1>this is Companies Page</h1>
+    </div>
+  );
 };
 
 export default companiesPage;
