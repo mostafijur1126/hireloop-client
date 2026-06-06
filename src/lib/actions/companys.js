a2bed4a70277862e60a77e169c0fa820d7e4d7a4
mostafijur1126 <mostafijurrhaman87@gmail.com>
2026-06-07 03:06:57 +0600

    add company information

--- src/lib/actions/companys.js
+++ src/lib/actions/companys.js
@@ -1,14 +1,20 @@
 "use server";
 
-const baseUrl = process.env.NEXT_PUBLIC_BASE_URL;
+import { serverMutation } from "../core/server";
 
 export const createCompany = async (newCompanyData) => {
-  const res = await fetch(`${baseUrl}/api/companies`, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(newCompanyData),
-  });
-  return res.json();
+  return serverMutation("/api/companies", newCompanyData);
 };
+
+// const baseUrl = process.env.NEXT_PUBLIC_BASE_URL;
+
+// export const createCompany = async (newCompanyData) => {
+//   const res = await fetch(`${baseUrl}/api/companies`, {
+//     method: "POST",
+//     headers: {
+//       "Content-Type": "application/json",
+//     },
+//     body: JSON.stringify(newCompanyData),
+//   });
+//   return res.json();
+// };
--- src/lib/actions/jobs.js
+++ src/lib/actions/jobs.js
@@ -1,12 +1,20 @@
 "use server";
-const baseUrl = process.env.NEXT_PUBLIC_BASE_URL;
+
+import { serverMutation } from "../core/server";
+
 export const createjob = async (newjob) => {
-  const res = await fetch(`${baseUrl}/api/jobs`, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(newjob),
-  });
-  return res.json();
+  return serverMutation("/api/jobs", newjob);
 };
+
+// const baseUrl = process.env.NEXT_PUBLIC_BASE_URL;
+
+// export const createjob = async (newjob) => {
+//   const res = await fetch(`${baseUrl}/api/jobs`, {
+//     method: "POST",
+//     headers: {
+//       "Content-Type": "application/json",
+//     },
+//     body: JSON.stringify(newjob),
+//   });
+//   return res.json();
+// };
